ff89330f7220ea6306740177a98ddfdca73b41e2
Adam Schmideg <adam.schmideg@prezi.com>
2014-09-26 21:26:10 +0200

    Add sample test file to test meditate

---
+++ test/koanize/sample_test.clj
@@ -0,0 +1,16 @@
+(ns koanize.sample-test
+  (:require [clojure.test :refer :all]))
+
+(deftest first-failure
+  (testing "0 == 1" (is (= 0 1)))
+  (testing "1 == 1" (is (= 1 1)))
+  (testing "1 == 0" (is (= 1 0)))
+  )
+
+(deftest pass
+  (testing "I am good"
+    (is (= 1 1))))
+
+(deftest second-failure
+  (testing "you shouldn't see me"
+    (is (= 0 2))))
