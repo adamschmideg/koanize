4adf8a3da519444fd287c8646f3fad7dacba8678
Adam Schmideg <adam.schmideg@prezi.com>
2014-09-26 21:24:23 +0200

    test-all-vars-until-successful

--- src/koanize/core.clj
+++ src/koanize/core.clj
@@ -5,13 +5,27 @@
 (defn report2
   [m]
   (case (:type m)
-    :fail (throw (Exception. (str "failed" )))
+    :fail (do (println "debug" *report-counters*) "just fail")
     :error (do (println m) (throw (Exception. (str "error" ))))
     nil))
 
+(defn test-all-vars-until-successful
+  "Calls test-var on every var interned in the namespace, with fixtures."
+  {:added "1.1"}
+  [ns]
+  (let [once-fixture-fn (join-fixtures (::once-fixtures (meta ns)))
+        each-fixture-fn (join-fixtures (::each-fixtures (meta ns)))]
+    (once-fixture-fn
+      (fn []
+        (doseq [v (vals (ns-interns ns))]
+          (when (and (:test (meta v))
+                     (successful? @*report-counters*))
+            (each-fixture-fn (fn [] (test-var v)))))))))
+
 (defn meditate
   [& namespaces]
-  (with-redefs [report report2]
+  (with-redefs [test-all-vars test-all-vars-until-successful]
     (try
+      ;(apply require namespaces)
       (apply run-tests namespaces)
       (catch Exception e (println e)))))
--- test/koanize/core_test.clj
+++ test/koanize/core_test.clj
@@ -1,17 +1,11 @@
 (ns koanize.core-test
   (:require [clojure.test :refer :all]
-            [koanize.core :refer :all]))
+            [koanize.core :refer :all]
+            ))
 
-(deftest a-test
+(deftest meditate-test
   (testing "FIXME, I fail."
-    (is (= 0 1))))
-
-(deftest good-test
-  (testing "I am good"
-    (is (= 1 1))))
-
-(deftest b-test
-  (testing "you shouldn't see me"
-    (is (= 0 1))))
+    (is (= 0 1))
+    ))
 
 ; (meditate)
